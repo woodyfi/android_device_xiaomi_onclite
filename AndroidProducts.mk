29314103253fe1ffe48e7995687b2be49719c454
ELbee <85691313+woodyfi@users.noreply.github.com>
2024-02-19 11:51:15 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/afterlife_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    afterlife_lavender-user \
+    afterlife_lavender-userdebug \
+    afterlife_lavender-eng
